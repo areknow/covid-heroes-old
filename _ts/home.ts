700cda1b757bbb7f93c2a23f50465fbd87a2ee3f
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2020-03-26 21:01:55 -0400

    function test

--- _ts/home.ts
+++ _ts/home.ts
@@ -21,15 +21,21 @@ interface FormFieldValues {
   });
 
   $('#form').submit(function(event) {
-    const fields = $(this).serializeArray();
-    const list = fields.filter(item => item.name === 'list').map(item => item.value);
-    const values: FormFieldValues[] = fields.filter(item => item.name !== 'list');
-    values.push({
-      name: 'list',
-      value: list,
+    // const fields = $(this).serializeArray();
+    // const list = fields.filter(item => item.name === 'list').map(item => item.value);
+    // const values: FormFieldValues[] = fields.filter(item => item.name !== 'list');
+    // values.push({
+    //   name: 'list',
+    //   value: list,
+    // });
+    // console.log(values);
+
+    const $form = $(this);
+    console.log($form)
+    $.post("https://covidheroes.gives/.netlify/functions/email", $form.serialize()).then(() => {
+      alert("Thank you!");
     });
-    console.log(values)
-    // event.preventDefault();
+    event.preventDefault();
   });
 
 })();
