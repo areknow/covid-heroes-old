b72a14e27a81e9374d89dc321418c861c5b8a7a3
Crowther, Arnaud <arnaud.crowther@dynatrace.com>
2020-03-27 10:46:47 -0400

    form validation

--- _ts/common.ts
+++ _ts/common.ts
@@ -1,70 +1,31 @@
-// ====================================================================
-// Common - js
-//
-// Scripts for the entire app
-// ====================================================================
-
 import * as $ from 'jquery';
 
 (() => {
 
-  /**
-   * Document ready
-   */
   $(() => {
     revealPage();
 
     const anchorlinks: NodeListOf<Element> = document.querySelectorAll('a[href^="#"]');
     for (const item of anchorlinks) {
-      item.addEventListener('click', (e) => {
-        const hashval = item.getAttribute('href')
-        const target = document.querySelector(hashval)
+      item.addEventListener('click', (event) => {
+        const hashval = item.getAttribute('href');
+        const target = document.querySelector(hashval);
         target.scrollIntoView({
           behavior: 'smooth',
           block: 'start'
-        })
-        history.pushState(null, null, hashval)
-        e.preventDefault()
+        });
+        history.pushState(null, null, hashval);
+        event.preventDefault();
       });
     }
   });
-
-  /**
-   * Reveal the page with a short delay
-   */
-  function revealPage() {
-    setTimeout(() => {
-      $('.page-loader-cover').fadeOut(500, () => {
-        $('.page-loader-cover').remove();
-      });
-    }, 2000);
-  }
 
 })();
 
-
-/**
- * Validate form fields
- * @param elemClass - the selected element to validate
- */
-function formValidation(elemClass: Element) {
-  // Set form to valid
-  let valid = true;
-  // Loop through form inputs
-  $(elemClass).each((index, elem) => {
-    // Check if empty
-    if ($(elem).val() === '' || $(elem).val() == null) {
-      // Show label
-      $(elem).prev('label').show();
-      // Set form to invalid
-      valid = false;
-    } else {
-      // Hide label
-      $(elem).prev('label').hide();
-    }
-  });
-  // Return true and submit form if valid
-  if (valid) {
-    return true;
-  }
+function revealPage() {
+  setTimeout(() => {
+    $('.page-loader-cover').fadeOut(500, () => {
+      $('.page-loader-cover').remove();
+    });
+  }, 2000);
 }
--- _ts/home.ts
+++ _ts/home.ts
@@ -1,9 +1,3 @@
-// ====================================================================
-// Home - js
-//
-// Scripts for the home page
-// ====================================================================
-
 import * as $ from 'jquery';
 
 interface FormFieldValues {
@@ -11,21 +5,51 @@ interface FormFieldValues {
   value: string | string[];
 }
 
+const ENDPOINT = "https://covidheroes.gives/.netlify/functions/email";
+let submissionAttempted = false;
+
 (() => {
 
-  $('#form button').click(() => {
+  $('#form button').click((event) => {
     event.preventDefault();
-    const fields = $('#form').serializeArray();
-    const list = fields.filter(item => item.name === 'list').map(item => item.value);
-    const values: FormFieldValues[] = fields.filter(item => item.name !== 'list');
-    values.push({
-      name: 'list',
-      value: list,
-    });
-    $.post("https://covidheroes.gives/.netlify/functions/email", JSON.stringify(values)).then(() => {
-      $('#form button').text('THANK YOU').prop('disabled', true);
-      $('#form').addClass('disabled');
-    });
+    submissionAttempted = true;
+    if (formValidation($('#form input:required'))) {
+      processForm();
+    }
+  });
+
+  $('#form input').change(() => {
+    if (submissionAttempted) {
+      formValidation($('#form input:required'));
+    }
   });
 
 })();
+
+async function processForm() {
+  const fields = $('#form').serializeArray();
+  const list = fields.filter(item => item.name === 'list').map(item => item.value);
+  const values: FormFieldValues[] = fields.filter(item => item.name !== 'list');
+  values.push({ name: 'list', value: list });
+  try {
+    await $.post(ENDPOINT, JSON.stringify(values));
+    $('#form button').text('THANK YOU').prop('disabled', true);
+    $('#form').addClass('disabled');
+  } catch (error) {
+    $('#form').hide();
+    $('.form-error').show();
+  }
+}
+
+function formValidation(elemClass: JQuery<HTMLElement>) {
+  let valid = true;
+  for (const elem of $(elemClass)) {
+    if ($(elem).val() === '' || $(elem).val() === null) {
+      $(elem).addClass('error');
+      valid = false;
+    } else {
+      $(elem).removeClass('error');
+    }
+  }
+  return valid;
+}
